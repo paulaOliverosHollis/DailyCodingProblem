2033bac5816fee577111713a494a38cdcba4aeba
Paula Oliveros Hollis <paulahollis21@gmail.com>
2020-02-08 15:03:37 -0500

    Added the solution to problem 2.

--- DailyCodingProblem/Program.cs
+++ DailyCodingProblem/Program.cs
@@ -8,6 +8,8 @@ namespace DailyCodingProblem
         static void Main(string[] args)
         {
             Problem1();
+
+            Problem2();
         }
 
         /// <summary>
@@ -41,6 +43,52 @@ namespace DailyCodingProblem
             }
 
             Console.WriteLine($"Sorry! There is not two numbers in the list that add up to {number}");
+        }
+
+
+        /// <summary>
+        /// Given an array of integers, return a new array such that each element of the new array is the product of all the numbers in the original array except the one at the current index.
+        /// </summary>
+        static void Problem2()
+        {
+            Console.WriteLine("I need you to give me at least five integers. Please enter one at the time!");
+            List<int> listOfNumbers = new List<int>();
+
+            while(listOfNumbers.Count < 5)
+            {
+                Console.Write("Please enter a number: ");
+                int number;
+                
+                while(!int.TryParse(Console.ReadLine(), out number))
+                {
+                    Console.Write("Your input is not valid. Please enter a integer: ");
+                }
+
+                listOfNumbers.Add(number);
+                
+            }
+
+            List<int> listOfProducts = new List<int>();
+
+            for (int i = 0; i < listOfNumbers.Count; i++)
+            {
+                int element = listOfNumbers[i];
+                listOfNumbers.RemoveAt(i);
+                int product = 1;
+                
+                for (int j = 0; j < listOfNumbers.Count; j++)
+                {
+                    product *= listOfNumbers[j];
+                }
+
+                listOfProducts.Add(product);
+                listOfNumbers.Insert(i, element);
+            }
+
+            foreach (int number in listOfProducts)
+            {
+                Console.Write($"{number} ");
+            }
         }
     }
 }
